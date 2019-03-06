99ff04690e60ac1715b08ab9e58077f444f5dbfb
Chun Zheng <chun.zheng@bronto.com>
2019-03-05 23:38:20 -0500

    day2

---
+++ python3/day02/part1.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from functools import reduce
+import operator
+
+def compute_checksum():
+    data = []
+    with open('input.txt', 'r') as f:
+        data = f.readlines()
+
+    numbers = []
+    for line in data:
+        count = set(Counter(line).values())
+        if 1 in count:
+            count.remove(1)
+        numbers.append(count)
+    nums = {}
+    for count in numbers:
+        for i in count:
+            if i in nums.keys():
+                nums[i] = nums[i] + 1
+            else: 
+                nums[i] = 1
+    print(reduce(operator.mul, nums.values()))
+
+
+compute_checksum()
